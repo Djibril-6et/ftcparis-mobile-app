17d8313f84d55068114122d67a4894cf8cd5fb7e
Djibril-6et <djibril.6et@gmail.com>
2025-07-28 20:54:08 +0200

    ScrollView with little bug

--- src/components/BottomListModal/BottomListModal.tsx
+++ src/components/BottomListModal/BottomListModal.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useThemeContext } from '../../context/ThemeContext';
 import { getStyles } from './BottomListModal.styles';
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, ScrollView } from 'react-native';
 import Modal from 'react-native-modal';
+import EventCard from '../EventCard/EventCard';
 
 type Props = {
   visible: boolean;
@@ -13,6 +14,20 @@ type Props = {
 const BottomListModal = ({ visible, onOpen, onClose }: Props) => {
   const { isDark } = useThemeContext();
   const styles = getStyles(isDark);
+
+  const events = [
+    { title: "Conférence Tech 2025" },
+    { title: "Atelier React Native" },
+    { title: "Hackathon Étudiant" },
+    { title: "Webinar : UX Design" },
+    { title: "Networking Startup" },
+    { title: "Salon de l’Innovation" },
+    { title: "Coding Challenge Paris" },
+    { title: "Masterclass en Cybersécurité" },
+    { title: "Festival Digital Nomad" },
+    { title: "Conférence sur l’IA Responsable" }
+  ];
+  
   return (
     <>
       {/* Modal preview button when it's closed */}
@@ -37,7 +52,19 @@ const BottomListModal = ({ visible, onOpen, onClose }: Props) => {
       >
         <View style={styles.content}>
           <View style={styles.handle} />
-          <Text style={styles.title}>Where are we going ?</Text>
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>Where are we going ?</Text>
+          </View>
+          <View style={styles.filterContainer}>
+            <View style={styles.filterItem}/>
+            <View style={styles.filterItem}/>
+            <View style={styles.filterItem}/>
+          </View>
+          <ScrollView style={styles.scrollView}>
+            {events && events.map(event => 
+              <EventCard title={event.title} key={event.title} />
+            )}
+          </ScrollView>
         </View>
       </Modal>
     </>
